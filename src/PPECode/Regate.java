5eb157bc4a84e879236fd7ad04df541ff9d32572
hirschmann yann <hirschmann.yann.bts.sio@gmail.com>
2017-11-21 12:05:24 +0100

    ajout classe regate yann

--- src/PPECode/Regate.java
+++ src/PPECode/Regate.java
@@ -3,6 +3,10 @@ package PPECode;
 public class Regate {
 	private int distMilles;
 
+	public Regate(int distMilles){
+		this.distMilles = distMilles;
+	}
+	
 	public int getDistMilles() {
 		return distMilles;
 	}
