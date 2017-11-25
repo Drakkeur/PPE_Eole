ac55d10d3d09ca9947cff7030e2834ac675716b1
MadJoke <MadJoke@192.168.1.13>
2017-11-25 15:00:03 +0100

    integration chronos

--- src/PPECode/fGestionRegate2.java
+++ src/PPECode/fGestionRegate2.java
@@ -14,14 +14,18 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JTable;
 
-
-public class fGestionRegate2 extends JFrame {
-
+public class fGestionRegate2 extends JFrame implements Runnable {
+	Thread t;
+	int hcnt=0,mcnt=0,scnt=0,mscnt =0;
+	String str = "", nstr="",mstr="",dstr="";
+	int cnt =0,cnt2=0;
 	private JPanel contentPane;
 	EnregistrementVoilier enr = new EnregistrementVoilier();
 	public static JTable tbleCandidat;
 	private static DefaultTableModel mdle;
 	private JTextField tfDistReg;
+	JLabel tmpsMs = new JLabel("000");
+	JLabel tmpsHMS = new JLabel("00:00:00");
 	
 	
 	public static DefaultTableModel getMdle() {
@@ -52,7 +56,7 @@ public class fGestionRegate2 extends JFrame {
 	 * Create the frame.
 	 */
 	public fGestionRegate2() {
-		
+		t = new Thread(this);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 615, 445);
 		contentPane = new JPanel();
@@ -61,64 +65,66 @@ public class fGestionRegate2 extends JFrame {
 		contentPane.setLayout(null);
 		
 		JLabel lblCandidat = new JLabel("Candidats");
-		lblCandidat.setBounds(313, 314, 56, 14);
+		lblCandidat.setBounds(313, 291, 56, 14);
 		contentPane.add(lblCandidat);
 		
-		JLabel lblCourseFin = new JLabel("Course Fini");
-		lblCourseFin.setHorizontalAlignment(SwingConstants.CENTER);
-		lblCourseFin.setBounds(505, 314, 84, 14);
-		contentPane.add(lblCourseFin);
-		
-		JButton btnArriv = new JButton("Arriver");
-		btnArriv.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				
-			}
-		});
-		btnArriv.setBounds(500, 339, 89, 23);
-		contentPane.add(btnArriv);
 		
 		JButton btnAjouter = new JButton("Ajouter");
 		btnAjouter.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				EnregistrementVoilier f = new EnregistrementVoilier();
-				
 				f.setVisible(true);
 			}
 		});
-		btnAjouter.setBounds(289, 339, 101, 23);
+		btnAjouter.setBounds(289, 310, 101, 23);
 		contentPane.add(btnAjouter);
 		
-		JLabel lblChrnomtre = new JLabel("Chronom\u00E8tre");
+		JLabel lblChrnomtre = new JLabel("Chronometre");
 		lblChrnomtre.setHorizontalAlignment(SwingConstants.CENTER);
 		lblChrnomtre.setBounds(400, 314, 95, 14);
 		contentPane.add(lblChrnomtre);
 		
-		JButton btnDmarer = new JButton("D\u00E9marer");
+		JButton btnDmarer = new JButton("Demarer");
 		btnDmarer.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
+				if(btnDmarer.getText().equals("Demarer")){
+					btnDmarer.setText("Arriver");
+					cnt++;
+					if(cnt==1){
+						t.start();
+						
+					}else{
+						t.resume();
+						
+					}
+				}else{
+					btnDmarer.setText("Arriver");
+					//t.suspend();
+					setTime();
+				}
 			}
 		});
 		btnDmarer.setBounds(400, 339, 89, 23);
 		contentPane.add(btnDmarer);
 		
-		JButton btnArret = new JButton("Arr\u00EAter");
+		JButton btnArret = new JButton("Arreter");
 		btnArret.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
+				reset();
+				t.suspend();
+				btnDmarer.setText("Demarer");
 			}
 		});
 		btnArret.setBounds(400, 373, 90, 23);
 		contentPane.add(btnArret);
-		
-		JButton btnRsultats = new JButton("R\u00E9sultats");
+
+		JButton btnRsultats = new JButton("Resultats");
 		btnRsultats.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				
 			}
 		});
-		btnRsultats.setBounds(500, 373, 89, 23);
+		btnRsultats.setBounds(289, 373, 101, 23);
 		contentPane.add(btnRsultats);
 		
 		JButton btnQuitter = new JButton("Quitter");
@@ -158,10 +164,10 @@ public class fGestionRegate2 extends JFrame {
 				}
 			}
 		});
-		btnSupprimer.setBounds(289, 373, 101, 23);
+		btnSupprimer.setBounds(289, 339, 101, 23);
 		contentPane.add(btnSupprimer);
 		
-		JButton btnEntr = new JButton("Entr\u00E9");
+		JButton btnEntr = new JButton("Entrer");
 		btnEntr.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				Regate r = new Regate(Integer.parseInt(tfDistReg.getText()));
@@ -175,11 +181,104 @@ public class fGestionRegate2 extends JFrame {
 		contentPane.add(tfDistReg);
 		tfDistReg.setColumns(10);
 		
-		JLabel lblDistanceRgate = new JLabel("Distance R\u00E9gate");
+		JLabel lblDistanceRgate = new JLabel("Distance Regate");
 		lblDistanceRgate.setBounds(10, 343, 114, 14);
 		contentPane.add(lblDistanceRgate);
 		
+		//Chrono afficheur
+		tmpsHMS.setBounds(20, 282, 79, 32);
+		contentPane.add(tmpsHMS);
+	
+		tmpsMs.setBounds(134, 282, 46, 32);
+		contentPane.add(tmpsMs);
+	}
+	
+	
+	
+	
+	public void display(){
+		tmpsHMS.setText(nstr);
+		tmpsMs.setText(mstr);
 		
+	}
+	public void reset(){
+		hcnt=0;mcnt=0;scnt=0;mscnt=0;cnt2=0;
+		nstr="00:00:00";mstr="000";dstr="";
+		display();
+	}
+	
+	public void setTimeCounter(){
+		nstr="";
+		if(hcnt<10){
+			nstr="0"+hcnt;
+			
+		}else{
+			nstr=""+hcnt;
+		}
+		
+		if(mcnt<10){
+			nstr+=":0"+mcnt;
+		}else{
+			nstr+=":"+mcnt;
+		}
+		if(scnt<10){
+			nstr+=":0"+scnt;
+		}else{
+			nstr+=":"+scnt;
+		}
+		
+	}
+	
+	public void setMTimeCounter(){
+		mstr="";
+		if(mscnt<10){
+			mstr= "00"+mscnt;
+		}else if (mscnt>=10 && mscnt<100){
+			mstr="0"+mscnt;
+		}else{
+			mstr=""+mscnt;
+		}
+	}
+	
+	public void setTime(){
+		dstr=tmpsHMS.getText()+" "+tmpsMs.getText();
+		tbleCandidat.setValueAt(dstr,tbleCandidat.getSelectedRow(),5);
+		
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		try{
+			while(true){
+				mscnt++;
+				if(mscnt>999){
+					mscnt=0;
+					scnt++;
+				}
+					
+				if(scnt>59){
+					scnt =0;
+					mcnt++;
+				}
+				if(mcnt>59){
+					mcnt=0;
+					hcnt++;
+					
+				}
+				if(hcnt>99){
+					reset();
+				}
+				setTimeCounter();
+				setMTimeCounter();
+				display();
+				Thread.sleep(1);
+				
+				
+			}
+		}catch(Exception e){
+			
+		}
 		
 	}
 }
